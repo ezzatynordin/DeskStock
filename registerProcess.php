2e9f6c8a0064de007602c02c89f84eafff3d6441
aimanrozali <aimanrozali@student.usm.my>
2023-04-18 00:54:01 +0800

    some comments

--- registerProcess.php
+++ registerProcess.php
@@ -1,6 +1,10 @@
 <?php
+
+// Include database configuration here
 include 'config.php';
 
+
+// Here we check if email and password is not null (passed from the form properly)
 if (isset($_POST['email']) && isset($_POST['password'])) {
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -8,20 +12,30 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     // $email = "ezzatybusuk@gmail.com";
     // $password = "1234";
 
+    // Debugging purpose
     echo '$email';
     echo '$password';
 
+    // Do a query here to check if the email already present or not
     $sql = "SELECT * FROM [deskStock].[users] WHERE email = ?";
     $val = array($email);
 
+    // Query to database
     if (sqlsrv_query($conn, $sql, $val)) {
         echo 'Email address already registered';
     } else {
+        //If email not present, then insert registration details to our database
         $sql = "INSERT INTO [deskStock].[users] (email, password) VALUES (?,?)";
+
+        // This params is responsible to pass value to the query above with ? mark. Why use this method? To avoid sql injection
         $params = array($email, $password);
+
+        // Initiate query to database
         if (sqlsrv_query($conn, $sql, $params)) {
+            // If success, display this message.
             echo 'Registration successful. <a href="login.php">Click here to log in.</a>';
         } else {
+            // If error occured when querying the database, display the error for debugging purpose.
             if (($errors = sqlsrv_errors()) != null) {
                 foreach ($errors as $error) {
                     echo "SQLSTATE: " . $error['SQLSTATE'] . "<br />";
